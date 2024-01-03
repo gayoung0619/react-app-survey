c9eb65d4394c1b88c1b963b05a182e8445b77bc8
gayoung0619 <did-619@naver.com>
2024-01-03 23:52:57 +0900

    feat: 플러팅바에 add버튼 추가

--- src/components/Layout/floatingBar.jsx
+++ src/components/Layout/floatingBar.jsx
@@ -1,14 +1,35 @@
 import * as React from 'react';
 import Fab from '@mui/material/Fab';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
+import {Link, useLocation} from 'react-router-dom';
 
 import classes from './floatingBar.module.css'
+import {v4 as uuidv4} from "uuid";
+import { addForm } from "../../slices/form.jsx";
+import { useDispatch } from "react-redux";
+import {useEffect} from "react";
 const FloatingBar = () => {
+	const location = useLocation();
+	const { pathname } = location;
+	const isPreview = pathname === '/preview';
+	const dispatch = useDispatch();
+
+	const handleAddButtonClick = () => {
+		const newItem = { id: `item-${uuidv4()}` };
+		dispatch(addForm(newItem));
+	};
+
 	return (
 		<div className={classes.floatingbar}>
-			<Fab color="primary"  aria-label="preview">
-				<RemoveRedEyeOutlinedIcon />
+			<Fab onClick={handleAddButtonClick}>
+				<AddCircleOutlineIcon />
 			</Fab>
+			<Link to='/preview'>
+				<Fab color="primary" aria-label="preview">
+					<RemoveRedEyeOutlinedIcon />
+				</Fab>
+			</Link>
 		</div>
 	)
 }
