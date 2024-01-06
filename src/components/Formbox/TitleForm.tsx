dfbbf01224fde3dfffd7edc806ba8e49cbb0075b
gayoung0619 <did-619@naver.com>
2024-01-06 19:17:26 +0900

    style: wrapper 하단 마진값수정

--- src/components/Formbox/TitleForm.tsx
+++ src/components/Formbox/TitleForm.tsx
@@ -12,76 +12,76 @@ const TitleForm = () => {
 	const location = useLocation();
 	const { pathname } = location;
 	const isPreview = pathname === '/preview';
-		const theme = useTheme();
-		const dispatch = useDispatch();
-		const title = useSelector((state: RootState) => state.title);
-		const handleTitleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-			dispatch(updateTitle(event.target.value));
-		}
+  const theme = useTheme();
+  const dispatch = useDispatch();
+  const title = useSelector((state: RootState) => state.title);
+  const handleTitleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    dispatch(updateTitle(event.target.value));
+  }
 
-		const handleDetailChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-			dispatch(updateDetail(event.target.value));
-		}
-		return (
-			<Box
-				component="form"
-				sx={{
-						'::before': {
-							content: '""',
-							display: 'block',
-							position: 'absolute',
-							top: 0,
-							left: 0,
-							width: '100%',
-							height: '10px',
-							backgroundColor: theme.palette.primary.main,
-							borderRadius: '8px 8px 0 0'
-						}
-					}}
-					>
-				{
-					isPreview ? (
-						<div className={classes.preview}>
-							<p className={classes.title} style={{ fontSize: '24pt', marginBottom: '8px' }}>{title.title}</p>
-							<p className={classes.detail}>{title.detail}</p>
-              <span
-                style={{
-                  display: 'inline-block',
-                  marginTop: '10px',
-                  color: 'red',
-                  fontSize: '13px',
-                }}>* 표시는 필수 질문임</span>
-						</div>
-					) : (
-						<>
-							<TextField
-								id="standard-basic"
-								variant="standard"
-								type="text"
-								className="inputs__title"
-								placeholder="제목 없는 설문지"
-								name="title"
-								fullWidth
-								inputProps={{ style: { fontSize: '2rem' } }}
-								sx={{ marginBottom: '8px' }}
-								onChange={handleTitleChange}
-								value={title.title}
-							/>
-							<TextField
-								id="standard-basic"
-								variant="standard"
-								type="text"
-								className="inputs__detail"
-								placeholder="설문지 설명"
-								name="detail"
-								fullWidth
-								onChange={handleDetailChange}
-								value={title.detail}
-							/>
-						</>
-					)
-				}
-			</Box>
-		)
-	}
+  const handleDetailChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    dispatch(updateDetail(event.target.value));
+  }
+  return (
+    <Box
+      component="form"
+      sx={{
+          '::before': {
+            content: '""',
+            display: 'block',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '10px',
+            backgroundColor: theme.palette.primary.main,
+            borderRadius: '8px 8px 0 0'
+          }
+        }}
+        >
+      {
+        isPreview ? (
+          <div className={classes.preview}>
+            <p className={classes.title} style={{ fontSize: '24pt', marginBottom: '8px' }}>{title.title}</p>
+            <p className={classes.detail}>{title.detail}</p>
+            <span
+              style={{
+                display: 'inline-block',
+                marginTop: '10px',
+                color: 'red',
+                fontSize: '13px',
+              }}>* 표시는 필수 질문임</span>
+          </div>
+        ) : (
+          <>
+            <TextField
+              id="standard-basic"
+              variant="standard"
+              type="text"
+              className="inputs__title"
+              placeholder="제목 없는 설문지"
+              name="title"
+              fullWidth
+              inputProps={{ style: { fontSize: '2rem' } }}
+              sx={{ marginBottom: '8px' }}
+              onChange={handleTitleChange}
+              value={title.title}
+            />
+            <TextField
+              id="standard-basic"
+              variant="standard"
+              type="text"
+              className="inputs__detail"
+              placeholder="설문지 설명"
+              name="detail"
+              fullWidth
+              onChange={handleDetailChange}
+              value={title.detail}
+            />
+          </>
+        )
+      }
+    </Box>
+  )
+}
 export default TitleForm;
